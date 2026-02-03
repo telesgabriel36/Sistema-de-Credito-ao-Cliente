e94404ee50df0c17b4289bcd805178654bc453a8
Gabriel Vieira Teles <telesgabriel36@gmail.com>
2026-02-03 14:17:53 -0300

    Commit inicial do projeto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Projeto_Credito_Cliente.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
